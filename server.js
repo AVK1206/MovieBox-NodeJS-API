ecf1dc6290a95c556266830cb7732e7414cc9840
Anatoliy <avk12061996@gmail.com>
2023-09-19 17:43:52 +0300

    Implemented endpoints to retrieve movies and movie details from the MongoDB database.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const { connectToDb, getDb } = require('./db');
-
+const { connectToDb, getDb } = require("./db");
+const { ObjectId } = require("mongodb");
+const express = require("express"); // Import the Express.js framework
 const PORT = 3000;
+
+const app = express();
+app.use(express.json()); // Middleware to parse JSON data in incoming requests
 
 let db;
 
@@ -15,7 +19,46 @@ connectToDb((err) => {
   }
 });
 
+
+// Define a function to handle errors and send an error response
 const handleError = (res, error) => {
   res.status(500).json({ error });
 }
 
+
+// Define a route to get a list of movies
+app.get("/movies", (req, res) => {
+  const movies = [];
+
+  db
+    .collection("movies")
+    .find()
+    .sort({ title: 1 }) // Sort movies by title in ascending order
+    .forEach((movie) => movies.push(movie)) // Iterate over the found movies and add them to the 'movies' array
+    .then(() => {
+      res
+        .status(200)
+        .json(movies);
+    })
+    .catch(() => handleError(res, "Something went wrong..."));
+});
+
+
+// Define a route to get details of a specific movie by ID
+app.get("/movies/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    const movieId = new ObjectId(req.params.id); // Convert the request parameter 'id' to a MongoDB ObjectId
+
+    db
+      .collection("movies")
+      .findOne({ _id: movieId })
+      .then((doc) => {
+        res
+          .status(200)
+          .json(doc);
+      })
+      .catch(() => handleError(res, "Something went wrong..."));
+  } else {
+    handleError(res, "Wrong id");
+  }
+});
